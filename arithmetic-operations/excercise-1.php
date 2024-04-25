ddca2a06b0a0df689cf525e9f3df042577d55e1e
discodexter <emils.timma@gmail.com>
2024-04-25 12:03:39 +0300

    second commit

--- arithmetic-operations/excercise-1.php
+++ arithmetic-operations/excercise-1.php
@@ -2,20 +2,21 @@
 
 /*Exercise #1
 Write a program to accept two integers and return true if the either one is 15 or if their sum or difference is 15.*/
-$intX = 15;
-$intY= 30;
 
-$intSum = $intX+$intY;
-$intDiff = abs($intX-$intY);
 
-if($intX === 15 || $intY ===15 || $intSum=== 15 || $intDiff ===15)
+function checkSum($intX,$intY)
 {
-    echo 'true'.PHP_EOL;
-} else {
-    echo "false".PHP_EOL;
+    $intSum = $intX + $intY;
+    $intDiff = abs($intX - $intY);
+
+    if ($intX === 15 || $intY === 15 || $intSum === 15 || $intDiff === 15) {
+        echo 'true' . PHP_EOL;
+    } else {
+        echo "false" . PHP_EOL;
+    }
+
+    echo $intDiff . PHP_EOL;
+    echo $intSum . PHP_EOL;
 }
 
-
-
-echo $intDiff.PHP_EOL;
-echo $intSum.PHP_EOL;
+checkSum(10,5);
