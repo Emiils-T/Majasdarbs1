76d2e45dead398a5c16a1bf6af25f529d100fa2e
discodexter <emils.timma@gmail.com>
2024-05-02 14:10:37 +0300

    second commit

--- arrays/excercise-6.php
+++ arrays/excercise-6.php
@@ -36,43 +36,44 @@ function displayGame($maskedWord, $misses)
     echo PHP_EOL . PHP_EOL;
 }
 
-$maxTries= 3;
+$maxTries = 3;
 $tries = 0;
 while ($tries < $maxTries) {
 
     displayGame($maskedWord, $misses);
 
     $userInput = strtolower(readline('Enter letter: '));
-    $found=null;
-    if(strlen($userInput==1) && !in_array($userInput, $maskedWord) && !in_array($userInput, $misses)) {
-        $found=false;
-    }
+    $found = null;
+    if (strlen($userInput) == 1 && !in_array($userInput, $maskedWord) && !in_array($userInput, $misses)) {
+        $found = false;
 
-    for ($i = 0; $i < count($selectedWordArray); $i++) {
-        if ($selectedWordArray[$i] === $userInput) {
-            $maskedWord[$i] = $userInput;
-            $found = true;
+
+        for ($i = 0; $i < count($selectedWordArray); $i++) {
+            if ($selectedWordArray[$i] === $userInput) {
+                $maskedWord[$i] = $userInput;
+                $found = true;
+            }
         }
-    }
 
-    if (!$found) {
-        $misses[] = $userInput;
-        $tries++;
-    }
+        if (!$found) {
+            $misses[] = $userInput;
+            $tries++;
+        }
 
-    if ($tries == 3) {
-        displayGame($maskedWord, $misses);
-        echo 'You lose' . PHP_EOL;
-        echo 'The word was: ' . $selectedWord . PHP_EOL;
-        break;
+        if ($tries == 3) {
+            displayGame($maskedWord, $misses);
+            echo 'You lose' . PHP_EOL;
+            echo 'The word was: ' . $selectedWord . PHP_EOL;
+            break;
+        }
+        if ($maskedWord === $selectedWordArray) {
+            displayGame($maskedWord, $misses);
+            echo 'You win' . PHP_EOL;
+            break;
+        }
+    } else {
+        echo 'invalid input' . PHP_EOL;
     }
-    if ($maskedWord === $selectedWordArray) {
-        displayGame($maskedWord, $misses);
-        echo 'You win' . PHP_EOL;
-        break;
-
-    }
-
 }
 
 
